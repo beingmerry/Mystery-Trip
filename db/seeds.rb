5c6ba885e38130cedc4fe555578a96c2e9487770
Ben Merryman <ben.merryman@gmail.com>
2023-02-11 13:55:36 -0500

    🛑Enable: once db ready for data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# 2023-02-11 Added Seed file info from TypeScript project
+
+# puts "💫 Beginning trips seeding..."
+# Trip.create(destination:"Miami, FL", start_date:"2023-06-01", end_date:"2023-06-07", trip_review: "it's was amazing")
+# Trip.create(destination:"Africa", start_date:"2023-09-01", end_date:"2023-09-09", trip_review: "left a lot to be desired")
+# puts "🌞 Creating some Days"
+# Day.create(notes: "lots of things to do this day!", day_rating: nil, day_review: nil, weather_suggested: "sunny, above 50degF")
+# puts "📆 Adding some days to trips"
+# TripDay.create(trip: Trip.first, day: Day.first, review: "was more fun than previous reviews let on!")
+
+# puts "🛬 Seeding complete!"
+
